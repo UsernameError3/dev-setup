25b382ff99b2a4ca7b2fea253459a33fec219d8d
Alex Fleming <alexfleming421@gmail.com>
2022-07-05 03:44:47 -0500

    moving to local utils

--- utils/util.js
+++ utils/util.js
@@ -7,7 +7,7 @@ const buildCardJSON = () => {
     let fileOutputName = path.resolve('data/data.json');
 
     console.log('Importing: ', fileInputName);
-    console.log('Writing: ', fileOutputName);
+    console.log('Writing:   ', fileOutputName);
     const data = csvToJson.fieldDelimiter(',').formatValueByType().getJsonFromCsv(fileInputName);
     fs.writeFileSync(fileOutputName, JSON.stringify(data, null, 4));
     console.log('Finished Card JSON Export...');
