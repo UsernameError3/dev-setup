cbf3595cdfa75db3ef28765523aca37c38413aed
Alex Fleming <alexfleming421@gmail.com>
2022-07-05 04:39:35 -0500

    testing conversion

--- utils/util.js
+++ utils/util.js
@@ -3,11 +3,11 @@ const path = require('path')
 const csvToJson = require('convert-csv-to-json');
 
 const buildCardJSON = () => {
-    let fileInputName = path.resolve('data/data.csv'); 
-    let fileOutputName = path.resolve('data/data.json');
-
+    const fileInputName = path.resolve('data/data.csv'); 
+    const fileOutputName = path.resolve('data/data.json');
     console.log('Importing: ', fileInputName);
     console.log('Exporting: ', fileOutputName);
+    
     const data = csvToJson.fieldDelimiter(',').formatValueByType().getJsonFromCsv(fileInputName);
     fs.writeFileSync(fileOutputName, JSON.stringify(data, null, 4));
     console.log('Finished Card JSON Export...');
